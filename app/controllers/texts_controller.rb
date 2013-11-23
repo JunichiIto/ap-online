e1f2ac984f619a625bf325261c39225f9f1f0cbc
Junichi Ito <jit@sonicgarden.jp>
2013-11-23 12:59:17 +0900

    Create scaffold for conversion

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,6 +2,8 @@ class TextsController < ApplicationController
   def new
   end
   def create
+    @text = params[:text]
+    @ap_text = @text.upcase
     respond_to do |format|
       format.html { render action: :new }
     end
